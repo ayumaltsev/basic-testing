f2aa6339ddebaf3be143a679831efcfff054455e
Anton Maltsev <Anton.Maltsev@McKesson.com>
2025-05-08 15:16:46 +0300

    feat: add snapshot-testing and snapshots folder

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,30 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import {generateLinkedList} from './index';
+
+const testArray = [1, 2, 3];
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
-  test('should generate linked list from values 1', () => {
-    // Write your test here
-  });
+    // Check match by expect(...).toStrictEqual(...)
+    test('should generate linked list from values 1', () => {
+        const expectedOutput = {
+            value: 1,
+            next: {
+                value: 2,
+                next: {
+                    value: 3,
+                    next: {
+                        value: null,
+                        next: null,
+                    },
+                },
+            },
+        };
 
-  // Check match by comparison with snapshot
-  test('should generate linked list from values 2', () => {
-    // Write your test here
-  });
+        expect(generateLinkedList(testArray)).toStrictEqual(expectedOutput);
+    });
+
+    // Check match by comparison with snapshot
+    test('should generate linked list from values 2', () => {
+        const list = generateLinkedList(testArray);
+        expect(list).toMatchSnapshot();
+    });
 });
